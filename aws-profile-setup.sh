3bb7f80470b332d4a9108a6b1d6763e5e3c9392f
Vipul1991git <76104729+Vipul1991git@users.noreply.github.com>
2021-04-14 18:21:17 +0530

    Add files via upload

---
+++ aws-profile-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+export CROSS_ACCOUNT_ROLE="arn:aws:iam::075342103456:role/cross-account-role-devenv"
+echo $CROSS_ACCOUNT_ROLE
+
+CREDS=$(aws sts assume-role \
+--role-arn $CROSS_ACCOUNT_ROLE \
+--role-session-name $(date '+%Y%m%d%H%M%S%3N') \
+--duration-seconds 3600 \
+--query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+--output text)
+
+echo $CREDS
+
+export AWS_DEFAULT_REGION="us-east-1"
+echo "export AWS_DEFAULT_REGION=us-east-1"
+export AWS_ACCESS_KEY_ID=$(echo $CREDS | cut -d' ' -f1)
+echo "export AWS_ACCESS_KEY_ID=$(echo $CREDS | cut -d' ' -f1)"
+export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | cut -d' ' -f2)
+echo "export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | cut -d' ' -f2)"
+export AWS_SESSION_TOKEN=$(echo $CREDS | cut -d' ' -f3)
+echo "export AWS_SESSION_TOKEN=$(echo $CREDS | cut -d' ' -f3)"
+
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
